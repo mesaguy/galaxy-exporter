dc323fa3b8cb36d28531bd84c3705ace5272de51
Mesaguy <mesaguy@mesaguy.com>
2020-06-26 05:27:07 +0000

    Add setup.py and release info

---
+++ galaxy_exporter/__init__.py
@@ -0,0 +1,6 @@
+""" Prometheus exporter of Ansible Galaxy metrics
+"""
+
+__myname__ = 'galaxy-exporter'
+__description__ = 'Prometheus exporter of Ansible Galaxy metrics'
+__version__ = '0.4.0'
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+""" Install galaxy-exporter
+"""
+
+from setuptools import setup, find_packages
+
+from galaxy_exporter import __myname__, __description__
+from galaxy_exporter import __version__
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    author='Mesaguy',
+    author_email='mesaguy@mesaguy.com',
+    name=__myname__,
+    description=__description__,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        # Improves aiohttp performance
+        'aiodns >= 2.0.0,<3',
+        # For API calls to Ansible Galaxy
+        'aiohttp >= 3.6.2,<4',
+        'fastapi >= 0.58,<1',
+        # For exporting Prometheus metrics
+        'prometheus_client >= 0.8.0,<1',
+        'python-dateutil >= 2.8.1',
+        'uvicorn >= 0.11.5,<1',
+        ],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    tests_require=[
+        'pylint >= 2.5.0,<2.6',
+        'pytest >= 5.3.2,<6',
+        'pytest-asyncio >= 0.11.0,<1',
+        'pytest-cov >= 2.8.1,<2.9',
+        'pytest-pep8 >= 1.0.6,<2',
+        'pytest-pylint >= 0.17.0',
+        'pytest-runner >= 5.1,<5.2',
+        ],
+    url='https://github.com/mesaguy/galaxy-exporter',
+    version=__version__,
+)
